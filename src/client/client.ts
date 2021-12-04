c122206b352a8408e47ea37567640759c315e4da
brunnacroches <brunnacroches@gmail.com>
2021-12-04 12:45:15 -0300

    Descrição
    A luz Threejs Hemisphere é muito parecida com uma luz direcional, mas também com configurações para projetar a luz na direção reversa. Também demonstro o objeto auxiliar de luz Hemisphere.

--- src/client/client.ts
+++ src/client/client.ts
@@ -6,10 +6,10 @@ import { GUI } from 'dat.gui'
 const scene = new THREE.Scene()
 scene.add(new THREE.AxesHelper(5))
 
-const light = new THREE.DirectionalLight()
+const light = new THREE.HemisphereLight()
 scene.add(light)
 
-const helper = new THREE.DirectionalLightHelper(light)
+const helper = new THREE.HemisphereLightHelper(light, 5)
 scene.add(helper)
 
 const camera = new THREE.PerspectiveCamera(
@@ -26,7 +26,7 @@ document.body.appendChild(renderer.domElement)
 
 new OrbitControls(camera, renderer.domElement)
 
-// const planeGeometry = new THREE.PlaneGeometry(20, 10)//, 360, 180)
+// const planeGeometry = new THREE.PlaneGeometry(100, 10)
 // const plane = new THREE.Mesh(planeGeometry, new THREE.MeshPhongMaterial())
 // plane.rotateX(-Math.PI / 2)
 // //plane.position.y = -1.75
@@ -88,7 +88,8 @@ document.body.appendChild(stats.dom)
 
 const data = {
     color: light.color.getHex(),
-    mapsEnabled: true,
+    groundColor: light.groundColor.getHex(),
+    mapsEnabled: true
 }
 
 const gui = new GUI()
@@ -98,11 +99,13 @@ lightFolder.addColor(data, 'color').onChange(() => {
 })
 lightFolder.add(light, 'intensity', 0, 1, 0.01)
 
-const directionalLightFolder = gui.addFolder('THREE.DirectionalLight')
-directionalLightFolder.add(light.position, "x", -100, 100, 0.01)
-directionalLightFolder.add(light.position, "y", -100, 100, 0.01)
-directionalLightFolder.add(light.position, "z", -100, 100, 0.01)
-directionalLightFolder.open()
+const hemisphereLightFolder = gui.addFolder('THREE.HemisphereLight')
+hemisphereLightFolder.addColor(data, 'groundColor').onChange(() => { light.groundColor.setHex(Number(data.groundColor.toString().replace('#', '0x'))) });
+
+hemisphereLightFolder.add(light.position, "x", -100, 100, 0.01)
+hemisphereLightFolder.add(light.position, "y", -100, 100, 0.01)
+hemisphereLightFolder.add(light.position, "z", -100, 100, 0.01)
+hemisphereLightFolder.open()
 
 const meshesFolder = gui.addFolder('Meshes')
 meshesFolder.add(data, 'mapsEnabled').onChange(() => {
