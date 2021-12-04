a9c6002c2628de0e8522417652156dff60e74f3b
brunnacroches <brunnacroches@gmail.com>
2021-12-04 12:38:40 -0300

    Descrição
    Você pode criar seus próprios mipmaps personalizados para serem usados ​​quando os pixels de textura são desenhados, dependendo das distâncias da câmera e se o texel precisa ser reduzido ou ampliado.

--- src/client/client.ts
+++ src/client/client.ts
@@ -11,12 +11,7 @@ scene1.add(axesHelper1)
 const axesHelper2 = new THREE.AxesHelper(5)
 scene2.add(axesHelper2)
 
-const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-)
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 camera.position.z = 1
 
 const renderer = new THREE.WebGLRenderer()
@@ -28,8 +23,47 @@ new OrbitControls(camera, renderer.domElement)
 const planeGeometry1 = new THREE.PlaneGeometry()
 const planeGeometry2 = new THREE.PlaneGeometry()
 
-const texture1 = new THREE.TextureLoader().load('img/grid.png')
-const texture2 = new THREE.TextureLoader().load('img/grid.png')
+// const texture1 = new THREE.TextureLoader().load("img/grid.png")
+// const texture2 = new THREE.TextureLoader().load("img/grid.png")
+
+const mipmap = (size: number, color: string) => {
+    const imageCanvas = document.createElement("canvas") as HTMLCanvasElement
+    const context = imageCanvas.getContext("2d") as CanvasRenderingContext2D
+    imageCanvas.width = size
+    imageCanvas.height = size
+    context.fillStyle = "#888888"
+    context.fillRect(0, 0, size, size)
+    context.fillStyle = color
+    context.fillRect(0, 0, size / 2, size / 2)
+    context.fillRect(size / 2, size / 2, size / 2, size / 2)
+    return context.getImageData(0, 0, size, size)
+}
+
+const texture1 = new THREE.CanvasTexture(document.createElement("canvas"));
+texture1.mipmaps[0] = mipmap(128, '#ff0000');
+texture1.mipmaps[1] = mipmap(64, '#00ff00');
+texture1.mipmaps[2] = mipmap(32, '#0000ff');
+texture1.mipmaps[3] = mipmap(16, '#880000');
+texture1.mipmaps[4] = mipmap(8, '#008800');
+texture1.mipmaps[5] = mipmap(4, '#000088');
+texture1.mipmaps[6] = mipmap(2, '#008888');
+texture1.mipmaps[7] = mipmap(1, '#880088');
+texture1.repeat.set(5, 5);
+texture1.wrapS = THREE.RepeatWrapping;
+texture1.wrapT = THREE.RepeatWrapping;
+
+const texture2 = new THREE.CanvasTexture(document.createElement("canvas"));
+texture2.mipmaps[0] = mipmap(128, '#ff0000');
+texture2.mipmaps[1] = mipmap(64, '#00ff00');
+texture2.mipmaps[2] = mipmap(32, '#0000ff');
+texture2.mipmaps[3] = mipmap(16, '#880000');
+texture2.mipmaps[4] = mipmap(8, '#008800');
+texture2.mipmaps[5] = mipmap(4, '#000088');
+texture2.mipmaps[6] = mipmap(2, '#008888');
+texture2.mipmaps[7] = mipmap(1, '#880088');
+texture2.repeat.set(5, 5);
+texture2.wrapS = THREE.RepeatWrapping;
+texture2.wrapT = THREE.RepeatWrapping;
 
 const material1 = new THREE.MeshBasicMaterial({ map: texture1 })
 const material2 = new THREE.MeshBasicMaterial({ map: texture2 })
@@ -43,6 +77,7 @@ const plane2 = new THREE.Mesh(planeGeometry2, material2)
 scene1.add(plane1)
 scene2.add(plane2)
 
+
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight
@@ -53,26 +88,22 @@ function onWindowResize() {
 
 const options = {
     minFilters: {
-        NearestFilter: THREE.NearestFilter,
-        NearestMipMapLinearFilter: THREE.NearestMipMapLinearFilter,
-        NearestMipMapNearestFilter: THREE.NearestMipMapNearestFilter,
-        'LinearFilter ': THREE.LinearFilter,
-        'LinearMipMapLinearFilter (Default)': THREE.LinearMipMapLinearFilter,
-        LinearMipmapNearestFilter: THREE.LinearMipmapNearestFilter,
+        "NearestFilter": THREE.NearestFilter,
+        "NearestMipMapLinearFilter": THREE.NearestMipMapLinearFilter,
+        "NearestMipMapNearestFilter": THREE.NearestMipMapNearestFilter,
+        "LinearFilter ": THREE.LinearFilter,
+        "LinearMipMapLinearFilter (Default)": THREE.LinearMipMapLinearFilter,
+        "LinearMipmapNearestFilter": THREE.LinearMipmapNearestFilter
     },
     magFilters: {
-        NearestFilter: THREE.NearestFilter,
-        'LinearFilter (Default)': THREE.LinearFilter,
-    },
+        "NearestFilter": THREE.NearestFilter,
+        "LinearFilter (Default)": THREE.LinearFilter,
+    }
 }
 const gui = new GUI()
 const textureFolder = gui.addFolder('THREE.Texture')
-textureFolder
-    .add(texture2, 'minFilter', options.minFilters)
-    .onChange(() => updateMinFilter())
-textureFolder
-    .add(texture2, 'magFilter', options.magFilters)
-    .onChange(() => updateMagFilter())
+textureFolder.add(texture2, 'minFilter', options.minFilters).onChange(() => updateMinFilter())
+textureFolder.add(texture2, 'magFilter', options.magFilters).onChange(() => updateMagFilter())
 textureFolder.open()
 
 function updateMinFilter() {
@@ -96,17 +127,13 @@ function animate() {
 }
 
 function render() {
+
     renderer.setScissorTest(true)
 
     renderer.setScissor(0, 0, window.innerWidth / 2 - 2, window.innerHeight)
     renderer.render(scene1, camera)
 
-    renderer.setScissor(
-        window.innerWidth / 2,
-        0,
-        window.innerWidth / 2 - 2,
-        window.innerHeight
-    )
+    renderer.setScissor(window.innerWidth / 2, 0, window.innerWidth / 2 - 2, window.innerHeight)
     renderer.render(scene2, camera)
 
     renderer.setScissorTest(false)
