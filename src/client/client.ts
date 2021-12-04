2e0fa2cbd6054a3ddfa5a1c8364b782419905c63
brunnacroches <brunnacroches@gmail.com>
2021-12-04 09:07:45 -0300

    update 05

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,5 +1,6 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import Stats from 'three/examples/jsm/libs/stats.module'
 
 const scene = new THREE.Scene()
 
@@ -16,7 +17,7 @@ renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
 const controls = new OrbitControls(camera, renderer.domElement)
-controls.addEventListener('change', render) //this line is unnecessary if you are re-rendering within the animation loop
+//controls.addEventListener('change', render)
 
 const geometry = new THREE.BoxGeometry()
 const material = new THREE.MeshBasicMaterial({
@@ -35,18 +36,23 @@ function onWindowResize() {
     render()
 }
 
-// function animate() {
-//     requestAnimationFrame(animate)
+const stats = Stats()
+document.body.appendChild(stats.dom)
 
-//     cube.rotation.x += 0.01
-//     cube.rotation.y += 0.01
+function animate() {
+    requestAnimationFrame(animate)
 
-//     render()
-// }
+    cube.rotation.x += 0.01
+    cube.rotation.y += 0.01
+
+    render()
+
+    stats.update()
+}
 
 function render() {
     renderer.render(scene, camera)
 }
 
-//animate()
-render()
+animate()
+//render()
