f50d54efe035571e91379ab1b1f51b213ffb7e39
brunnacroches <brunnacroches@gmail.com>
2022-01-07 00:41:17 -0300

    subindo

--- index.js
+++ index.js
@@ -7,17 +7,8 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', (req, res) => res.render('index'))
   .get('/cool', (req, res) => res.send(cool()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-  get('/times', (req, res) => res.send(showTimes()))
-
-  showTimes = () => {
-    let result = '';
-    const times = process.env.TIMES || 5;
-    for (i = 0; i < times; i++) {
-      result += i + ' ';
-    }
-    return result;
-  }
+ 
