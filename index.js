3328df189bb2a1ad4f66ae7e1e3b551c7169c4e1
brunnacroches <brunnacroches@gmail.com>
2022-01-09 16:57:14 -0300

    a

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const PORT = process.env.PORT || 5000;
 
 express()
-  .use(express.static(path.join(__dirname, './dist')))
+  .use(express.static(path.join(__dirname,'../client')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('index'))
