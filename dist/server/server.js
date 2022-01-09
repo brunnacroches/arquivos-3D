9ae4b100dbdf0330c8f1f79d5589df23302ecd45
brunnacroches <brunnacroches@gmail.com>
2022-01-09 17:13:39 -0300

    add

--- dist/server/server.js
+++ dist/server/server.js
@@ -12,15 +12,6 @@ class App {
         this.port = port;
         const app = (0, express_1.default)();
         app.use(express_1.default.static(path_1.default.join(__dirname, '../client')));
-        // This server.ts is only useful if you are running this on a production server or you
-        // want to see how the production version of bundle.js works
-        //
-        // to use this server.ts
-        // # npm run build        (this creates the production version of bundle.js and places it in ./dist/client/)
-        // # tsc -p ./src/server  (this compiles ./src/server/server.ts into ./dist/server/server.js)
-        // # npm start            (this starts node.js with express and serves the ./dist/client folder)
-        //
-        // visit http://127.0.0.1:3000
         this.server = new http_1.default.Server(app);
     }
     Start() {
