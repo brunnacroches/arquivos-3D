eb93e535747edb3577906d03aa8609e839962301
brunnacroches <brunnacroches@gmail.com>
2022-01-09 17:03:22 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ express()
   .use(express.static(path.join(__dirname,'../client')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('../', (req, res) => res.render('index'))
+  .get('../', (req, res) => res.render('dist/index.html'))
   .get('/cool', (req, res) => res.send(cool()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
